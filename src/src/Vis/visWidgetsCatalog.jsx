1bb82086fc88a849a47c285b53f9a09b9e2c8bf1
foxriver76 <moritz.heusinger@gmail.com>
2024-02-05 11:20:59 +0100

    fixed issue, that Basic Image 8 is not configurable for 0 value
    - closes #354

--- src/src/Vis/visWidgetsCatalog.jsx
+++ src/src/Vis/visWidgetsCatalog.jsx
@@ -495,7 +495,9 @@ export const parseAttributes = (widgetParams, widgetIndex, commonGroups, commonF
         commonGroups = commonGroups || { common: 1 };
         commonFields = commonFields || {};
         const fields = deepClone(widgetParams);
-        let groupIndex = fields.findIndex(group => group.indexFrom);
+        let groupIndex = fields.findIndex(group => typeof group.indexFrom === 'number');
+
+        console.log(groupIndex);
         while (groupIndex > -1) {
             const group = fields[groupIndex];
             group.singleName = group.name;
@@ -518,6 +520,7 @@ export const parseAttributes = (widgetParams, widgetIndex, commonGroups, commonF
             delete group.indexFrom;
             delete group.indexTo;
             const indexedGroups = [];
+
             for (let i = from; i <= to; i++) {
                 const indexedGroup = {
                     ...deepClone(group),
